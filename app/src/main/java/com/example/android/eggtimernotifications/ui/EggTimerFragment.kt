39fdb3bb31d4abf5c31c8423716ae2c2b34ff6a9
Javier <javilena87@gmail.com>
2020-05-24 13:22:22 +0200

    Notification Badges

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -65,7 +65,9 @@ class EggTimerFragment : Fragment() {
                 channelName,
                 NotificationManager.IMPORTANCE_HIGH
             )
-            // TODO: Step 2.6 disable badges for this channel
+                .apply {
+                    setShowBadge(false)
+                }
 
             notificationChannel.enableLights(true)
             notificationChannel.lightColor = Color.RED
