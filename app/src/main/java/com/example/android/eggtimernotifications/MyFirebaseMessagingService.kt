150dcfa5b321fd2bb669256643a6a4d0b9b5cf51
Javier <javilena87@gmail.com>
2020-05-31 14:10:12 +0200

    Data Messages

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -2,8 +2,9 @@ package com.example.android.eggtimernotifications
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseMessagingService: FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     companion object {
         val TAG = MyFirebaseMessagingService::class.java.name
@@ -15,7 +16,16 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        sendRegistrationToServer(token)    }
+        sendRegistrationToServer(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        Log.d(TAG, "From: ${remoteMessage?.from}")
+
+        remoteMessage?.data?.let {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
+    }
 
     private fun sendRegistrationToServer(token: String?) {
     }
